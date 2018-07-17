661b6dbfeb59f76031cd4d85eda913f1f9694214
Manuel Rivas <mgrivas777@gmail.com>
2018-07-17 10:49:09 -0700

    Implemented 2-way communication for both browser and board

--- mBed_Board_Code/Browser_to_Board/board_to_browser_test.cpp
+++ mBed_Board_Code/Browser_to_Board/board_to_browser_test.cpp
@@ -5,32 +5,38 @@
 //USBSerial serial;
 DigitalOut  myled(LED1);
 
+/*
+bool write(uint8_t * buffer, uint32_t size, bool isCDC=false);
+bool read(uint8_t * buffer, uint32_t * size, bool isCDC=false, bool blocking=false);
+Keep these here to see the definitions
+*/
+#define MAX_BUF_SIZE 1024
+
 int main(void) {
-   // uint8_t buffer[] = {'3','.','1','4','1','5','9'};
-    //uint8_t buffer1[] = {'h','e','l','l','o'};
-    uint8_t readBuffer[32];
-    uint32_t * len;
-    *len = 32;
+    //uint8_t buffer[] = {'3','.','1','4','1','5','9'};
+    uint32_t len;
+    uint8_t * buf; //Pointer necessary for read function. Should be needed for write as well but works without pointer for some reason
+    
     WebUSBCDC usbDFU(0x1F00,0x2012,0x0001, false);
     while(1)
     {
-        //serial.printf("I am a virtual serial port\r\n");
-        wait(1);
+        buf = new uint8_t[MAX_BUF_SIZE];
         // Check the DFU status
         if (!usbDFU.configured()) {
             usbDFU.connect();
         }
-        /*if(usbDFU.configured() && usbDFU.write(buffer1,8))
+        /*
+        if(usbDFU.configured() && usbDFU.write(buffer,8))
         {
             wait(1);
-        }  */   
-        if(usbDFU.configured() && usbDFU.read(readBuffer, len, false, true))
-        {//MAY WANT TO CHECK IF TRUE SHOULD BE FALSE
-            wait(1);    
-        }  
-        if(usbDFU.configured() && usbDFU.write(readBuffer,32))
+        } 
+        */
+        pc.printf("Entering read\n\r");
+       if(usbDFU.configured() && usbDFU.read(buf, &len))
         {
             wait(1);
-        }
+            usbDFU.write(buf, 8);//Triesto write what it just read
+        } 
+    delete buf;
     }
 }
