8fc20c6d6a53c1e696b519fe42c7078d9b21f394
Fernando Francisco Cruz <fcruz@nesls-iMac.local>
2018-07-23 12:04:16 -0700

    Sig figs added for results, cleaned up code a bit

--- Simple_Server/testScript.js
+++ Simple_Server/testScript.js
@@ -3,8 +3,7 @@
     to select the interface. Await is followed with a promise. This means that the
     promise will carry out and the function will wait until that promised is fulfilled.
     You can store the value of this promise in a variable if desired.*/
-    async function connectDev(device)
-    {
+    async function connectDev(device){
         console.log('Opening...');
         //This function will wait until the device is open
         await device.open();
@@ -30,8 +29,7 @@
     //CHANGED
     /*This function is used to receive data from device, function will return 
     the data received*/
-    async function receiveData(device)
-    {
+    async function receiveData(device){
         //Make the device ready to receive data
         await device.controlTransferOut({
             requestType: 'class',
@@ -51,8 +49,7 @@
     }
     //-------------------------------------------------------------------
     /*This function is used to send data to the device, u_input is data to be sent*/
-    async function sendData(device, u_input)
-    {
+    async function sendData(device, u_input){
         //Make the device ready to send data
         await device.controlTransferIn({
             requestType: 'class',
@@ -78,14 +75,23 @@
 
     //-------------------------------------------------------------------
     /*This function is used to close the device*/
-    async function closeDev(device)
-    {
+    async function closeDev(device){
         console.log('Closing...')
         await device.close();
         if(device.opened)
             console.log('Device did not close');
         else
             console.log('Device closed');
+    }
+
+    //-------------------------------------------------------------------
+    /*This function disables/enables all buttons*/
+    function disableButtons(flag){
+        receive.disabled = flag;
+        send.disabled = flag;
+        run.disabled = flag;
+        blinky1.disabled = flag;
+        blinky2.disabled = flag;
     }
 
     //-------------------------------------------------------------------
@@ -110,9 +116,18 @@
         console.log('Device disconencted');// Remove |event.device| from the UI.
     });
 
-    //-------------------------------------------------------------------
-    /*Defining what happens when the button is clicked*/
+    //-------------------------------------------------------------------    
+    /*Define all buttons/variables beforehand*/
     let receive = document.getElementById('request-device');
+    let send = document.getElementById('send');
+    let blinky2 = document.getElementById('blinky2');
+    let blinky1 = document.getElementById('blinky1');
+    let run = document.getElementById('test-cases');
+    var SIG_FIG = 7;
+    //-------------------------------------------------------------------
+    /*Defining what happens when the receive button is clicked. This is meant
+    to receive data from the test board and print to console*/
+
     //Check that the button exists
     if(receive){
         //What happens when the button is clicked
@@ -120,6 +135,7 @@
             document.getElementById('demo').innerHTML = "Paragraph changed.";
             let device;
             try {
+                disableButtons(true);
                 //Have user select device
                 device = await navigator.usb.requestDevice({filters: [{vendorId:0x1F00}]})
                 //Connect the device
@@ -134,41 +150,54 @@
             } catch (err){
                 console.log('No Device was selected in receive');//No device was selected.
             }
+            disableButtons(false);
         })
     }
 
     //-------------------------------------------------------------------
-    /*Defines what happens when the send button is clicked*/
-    let send = document.getElementById('send');
+    /*Defines what happens when the send button is clicked. This is used to 
+    send period and duty cycle (user input) to the test board and then receive the time 
+    stamps measured by the test baord for the period and duty cycle.*/
     if(send){
         send.addEventListener('click', async() => {
             document.getElementById('demo').innerHTML = "Entered Here";
             let device;
+            //Get values period and duty cycle entered by usert
             var period = document.getElementById('per').value;
-            var dutyCycle = document.getElementById('dutyCycle').value;            try{
+            var dutyCycle = document.getElementById('dutyCycle').value;            
+            try{
+                //Select device
+                disableButtons(true);
                 device = await navigator.usb.requestDevice({filters: [{vendorId:0x1F00}]})
                 await connectDev(device);
+                //Send period and duty cycle to device
                 await sendData(device,period); //Currently, sendData also receives data to test it
                 await sendData(device,dutyCycle);
+                //Receive period and duty cycle measured from test board
                 await receiveData(device);
                 await receiveData(device);
                 await closeDev(device);
             } catch(err){
                 console.log('No Device was selected' + err);
             }
+            disableButtons(false);
         })
     }
 
     //-------------------------------------------------------------------
-    //Defines what happens when Download Main button is clicked
-    let blinky2 = document.getElementById('blinky2');
+    /*Defines what happens when Download Blinky2 button is clicked. This 
+    downloads a program that simply causes LED1 to blink. Transferring this file 
+    should cause the test baord to reset itself.*/
     if(blinky2){
         blinky2.addEventListener('click', async()=>{
             let device;
             try{
+                disableButtons(true);
+                //Select the device
                 device = await navigator.usb.requestDevice({filters: [{vendorId:0x1F00}]})
                 await connectDev(device);
                 console.log("blinky2");
+                //Send RESET to the testboard to have the testboard reset itself
                 await sendData(device, 'RESET');
                 console.log('sent');
                 await closeDev(device);
@@ -176,19 +205,24 @@
             catch(err){
                 console.log(err);
             }
+            disableButtons(false);
         })
     }
 
     //-------------------------------------------------------------------
-    /*Defines what happens when Download Blinky button is clicked*/
-    let blinky1 = document.getElementById('blinky1');
+    /*Defines what happens when Download Blinky button is clicked. This
+    downloads a program that simply causes LED3 to blink. Transferring this file 
+    should cause the test baord to reset itself.*/
     if(blinky1){
         blinky1.addEventListener('click', async()=>{
             let device;
             try{
+                disableButtons(true);
+                //Select the device
                 device = await navigator.usb.requestDevice({filters: [{vendorId:0x1F00}]})
                 await connectDev(device);
                 console.log("blinky1");
+                //Send RESET to test board to have it reset itself
                 await sendData(device, 'RESET');
                 console.log('sent');
                 await closeDev(device);
@@ -196,15 +230,19 @@
             catch(err){
                 console.log(err);
             }
+            disableButtons(false);
         })
     }
 
     //-------------------------------------------------------------------
+    /*This is used to several test cases to the test board. The test cases are sent
+    and then the time stamps are received by the test board. These time stamps are 
+    formatted to be displayed on the browser.*/
     //BUTTON FUNCTIONALITY
-     let run = document.getElementById('test-cases');
      if (run){
         run.addEventListener('click', async () => {
             let device;
+            //Define all periods and duty cycles in binary
             var per1 = '01111';
             var duty1 = '0110010';
             var per2 = '01011';
@@ -217,36 +255,48 @@
             var duty5 = '0010001';
             var list = [per1, duty1, per2, duty2, per3, duty3, per4, duty4, per5, duty5];
             var results = [];
-            var index = 1;
-            receive.disabled = true;
-            send.disabled = true;
-            run.disabled = true;
-            blinky1.disabled = true;
-            blinky2.disabled = true;
+            var index = 1;  //Keeps track of what test case we are on
+            disableButtons(true);
 
             try{
             device = await navigator.usb.requestDevice({filters: [{vendorId:0x1F00}]})
             for(var i=0; i<10; i+=2){
                 await connectDev(device);
+
+                //Send period followed by duty cycle to test board
                 await sendData(device, list[i]);
                 await sendData(device, list[i+1]);
+
+                //Store time stamp of period then duty cycle to results
                 results[i] = await receiveData(device);
                 results[i+1] = await receiveData(device);
-                var per = parseFloat(results[i]) * 1000;
-                var dCycle = parseFloat(results[i+1]) * 100;
-                var div = document.createElement('DIV');
-                var pdiv = document.createElement('DIV');
-                var dcdiv = document.createElement('DIV');
-                var br = document.createElement('BR');
+
+                //Convert results into floats
+                var per = (parseFloat(results[i]) * 1000).toFixed(SIG_FIG);; //Convert from seconds to ms
+                var dCycle = (parseFloat(results[i+1]) * 100).toFixed(SIG_FIG); //Convert from decimal to percentage
+
+                //Formatting
+                var div = document.createElement('DIV'); //Test Case div
+                var pdiv = document.createElement('DIV');  //Period div
+                var dcdiv = document.createElement('DIV');  //Duty Cycle div 
+                var br = document.createElement('BR'); //Used as <br>
+
+                //Print test case
                 var t = document.createTextNode('Test Case: ' + index);
                 div.appendChild(t);
                 document.body.appendChild(div);
+
+                //Print period
                 t = document.createTextNode(' Period received: ' + per  + 'ms');
                 pdiv.appendChild(t);
                 document.body.appendChild(pdiv);
+
+                //Print duty cycle
                 t = document.createTextNode(' Duty Cycle received: '+ dCycle +'%');
                 dcdiv.appendChild(t);
                 document.body.appendChild(dcdiv);
+
+                //Print <br>
                 document.body.appendChild(br);
                 index++;
             }
@@ -254,13 +304,9 @@
         catch(err){
             console.log(err);
         }
-            index = 1;
-            receive.disabled = false;
-            send.disabled = false;
-            run.disabled = false;
-            blinky1.disabled = false;
-            blinky2.disabled = false;
-            console.log(results.toString());
+            index = 1; //Reset index for next time
+            disableButtons(false);
+            console.log(results.toString()); //Print raw time stamps to console
 
 
         })
