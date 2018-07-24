a0b5dc64bb524384c97b4239987ac0b734a18ca5
Manuel Rivas <mgrivas777@gmail.com>
2018-07-24 10:50:37 -0700

    Removed useless code for efficiency

--- mBed_Board_Code/3_way_communication/LPC1768.cpp
+++ mBed_Board_Code/3_way_communication/LPC1768.cpp
@@ -2,7 +2,6 @@
 #include "USBSerial.h"
 #include "WebUSBCDC.h"
 
-#include <cmath>
 #include <string>
 #include <sstream>
 
@@ -44,8 +43,8 @@ DigitalOut dutyList[] = {MSD, DC2, DC3, DC4, DC5, DC6, LSD}; //List of duty cycl
 Serial pc(USBTX, USBRX);
 
 //Helper Function Prototypes; Definitions below main
-bool sendBinary(uint8_t buffer[], DigitalOut list[], int size);
-void time(float &period, float &duty, bool largeDuty);
+void sendBinary(uint8_t buffer[], DigitalOut list[], int size);
+void time(float &period, float &duty);
 void convertFloatToBuf(float num, uint8_t buf[], int sigFigs);
 void writeToBrowser(uint8_t buffer[]);
 void readDataFromBrowser(uint8_t * buffer, uint32_t & len);
@@ -57,19 +56,18 @@ int main()
 {
     while(1) 
     {
-        pc.printf("%f\n\r", RECORDING_TIME);
         float period = 0;
         float duty = 0;
         uint32_t lenPer, lenDuty; //Leave undeclared, or else read function will not work
         uint8_t * perBuf1, * dutyBuf1;
         perBuf1 = new uint8_t[MAX_BUF_SIZE]; //Dynamically allocated because read function prefers it
         dutyBuf1 = new uint8_t[MAX_BUF_SIZE];
-        readDataFromBrowser(perBuf1, lenPer);
+        readDataFromBrowser(perBuf1, lenPer); //Storing binary from browser into buffer
         readDataFromBrowser(dutyBuf1, lenDuty);
         sendBinary(perBuf1, perList, NUM_PERIOD_PINS); //Sends data for other board to interpret
-        bool isLarge = sendBinary(dutyBuf1, dutyList, NUM_DUTY_CYCLE_PINS);
+        sendBinary(dutyBuf1, dutyList, NUM_DUTY_CYCLE_PINS);
         req = 1; //Send signal for other board to generate waves
-        time(period, duty, isLarge); //Period and duty cycle will be returned by reference
+        time(period, duty); //Period and duty cycle will be returned by reference
         req = 0; //Set signal to 0 so that it can rise again
         delete [] perBuf1; //Deallocating memory
         delete [] dutyBuf1;
@@ -99,26 +97,22 @@ int main()
     }
 }
 //-----------------------------------------------------------
-bool sendBinary(uint8_t buffer[], DigitalOut list[], int size) //Function that sends signals to student board
+void sendBinary(uint8_t buffer[], DigitalOut list[], int size) //Function that sends signals to student board
 {
-    int dutySum = 0;
     for(int i=0; i<size; ++i)
     {
         if(buffer[i] == '1')
         {
-            list[i] = 1;
-            dutySum += pow(2.0,NUM_DUTY_CYCLE_PINS - 1 - i);
+            list[i] = 1;//Corresponding bit pin is ON
         }
         else
-            list[i] = 0;
+            list[i] = 0;//Corresponding bit pin is OFF
     }
-    return(dutySum > 50);
 }
 //-----------------------------------------------------------
-void time(float &period, float &duty, bool largeDuty) //Function that times board
+void time(float &period, float &duty) //Function that times board
 {
     Timer onTime;
-    Timer offTime;
     Timer totTime;
     Timer timeOut;
     int numCycles = 0;
@@ -138,9 +132,6 @@ void time(float &period, float &duty, bool largeDuty) //Function that times boar
             while(PWM_API.read()){}
             onTime.stop();
             numCycles += 1;
-            //period = totTime.read();
-            //duty = onTime.read()/period;
-            //totTime.stop();
         }
         totTime.stop();
         period = totTime.read() / numCycles;
@@ -151,24 +142,6 @@ void time(float &period, float &duty, bool largeDuty) //Function that times boar
         pc.printf("Error. Timeout occurred.\n\r");
         period = -1.0;
     }
-        /*
-        else
-        {
-            while(totTime.read() < RECORDING_TIME)
-            {
-                while(!PWM_API.read()){}
-                offTime.stop();
-                while(PWM_API.read()){}
-                offTime.start();
-                numCycles += 1;
-                //period = totTime.read();
-                //duty = onTime.read()/period;
-                //totTime.stop();
-            }
-            period = totTime.read() / numCycles;
-            duty = 1.0 - (offTime.read() / totTime.read());
-        }
-        */
 }
 //-----------------------------------------------------------
 void convertFloatToBuf(float num, uint8_t buf[], int sigFigs) //Converts a float to a uint8_t buffer to send
@@ -186,28 +159,28 @@ void writeToBrowser(uint8_t buffer[]) //Sends data to the browser
 {
     bool isFinished = false;
     while(!isFinished)
-            {
-                if(!webUSB.configured())
-                    webUSB.connect();
-                if(webUSB.configured() && webUSB.write(buffer,SIG_FIGS))
-                {
-                    isFinished = true;
-                }
-            }
+    {
+        if(!webUSB.configured())
+            webUSB.connect();
+        if(webUSB.configured() && webUSB.write(buffer,SIG_FIGS))
+        {
+            isFinished = true;
+        }
+    }
 }
 //----------------------------------------------------------
 void readDataFromBrowser(uint8_t *buffer, uint32_t & len) //Reads data from browser into buffer
 {   
     bool read = false;
-        while(!read)
+    while(!read)
+    {
+        if (!webUSB.configured()) 
         {
-            if (!webUSB.configured()) 
-            {
-                webUSB.connect();
-            }   
-            if(webUSB.configured() && webUSB.read(buffer, &len))
-            {
-                read = true;
-            } 
-        }
+            webUSB.connect();
+        }   
+        if(webUSB.configured() && webUSB.read(buffer, &len))
+        {
+            read = true;
+        } 
+    }
 }
